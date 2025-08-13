af31c75126c8142b4bfd58859a040a4628942859
Karl Knutsson <karl.fb.knutsson@gmail.com>
2025-08-13 15:59:13 +0200

    Add option for generating plutus TX load
    By setting TX_GEN_MODE to either plain or plutus one can control what
    type of load a tx-generator node generates.

--- cardano-node/scripts/tx-generator.sh
+++ cardano-node/scripts/tx-generator.sh
@@ -6,6 +6,10 @@ set -o pipefail
 SHELL="/bin/bash"
 PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
 
+# Environment variables
+TPS="${TSP:-1}"
+TX_GEN_MODE="${TX_GEN_MODE:-plain}"
+
 # Check if required environment variables are set
 if [[ -z "${POOL_ID}" ]]; then
     echo "Error: POOL_ID environment variable must be set" >&2
@@ -51,10 +55,62 @@ cat <<EOF > tx-generator.json
 }
 EOF
 
+cat <<EOF > tx-generator-plutus.json
+{
+  "add_tx_size": 100,
+  "debugMode": false,
+  "era": "Conway",
+  "init_cooldown": 45,
+  "inputs_per_tx": 1,
+  "localNodeSocketPath": "/opt/cardano-node/data/db/node.socket",
+  "min_utxo_value": 10000000,
+  "nodeConfigFile": "/opt/cardano-node/pools/${POOL_ID}/configs/config.json",
+  "outputs_per_tx": 1,
+  "sigKey": "/opt/cardano-node/utxos/keys/genesis.${POOL_ID}.skey",
+  "targetNodes": [
+    {
+      "addr": "127.0.0.1",
+      "name": "node-0",
+      "port": 3001
+    }
+  ],
+  "plutus": {
+    "datum": null,
+    "limitExecutionMem": null,
+    "limitExecutionSteps": null,
+    "redeemer": "plutus-redeemer.json",
+    "script": {
+      "Right": "/opt/tx-generator/scripts-fallback/Loop.plutus"
+    },
+    "type": "LimitSaturationLoop"
+  },
+  "tps": 0.85,
+  "tx_count": 61200,
+  "tx_fee": 1360000
+}
+EOF
+
+cat <<EOF > plutus-redeemer.json
+{
+  "int":1000000
+}
+EOF
+
 while [ ! -S /opt/cardano-node/data/db/node.socket ]; do
     echo "node.socket not found. Waiting 3 seconds..."
     sleep 3
 done
 
 # Launch the tx-generator process
-tx-generator json_highlevel tx-generator.json
+case $TX_GEN_MODE in
+  "plain")
+    tx-generator json_highlevel tx-generator.json
+    ;;
+  "plutus")
+    tx-generator json_highlevel tx-generator-plutus.json
+    ;;
+  *)
+    echo "Unknown TX_GEN_MODE ${TX_GEN_MODE}."
+    exit 1
+    ;;
+esac
